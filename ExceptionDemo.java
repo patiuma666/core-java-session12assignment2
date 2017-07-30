ae6b29998f900fdcf72ac1b214ab1c573dda69fb
patiuma666 <patiuma666@gmail.com>
2017-07-30 23:46:31 +0530

    Add files via upload

---
+++ ExceptionDemo.java
@@ -0,0 +1,36 @@
+
+package assignment12;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class ExceptionDemo { //i created class EXceptionDemo
+	
+	public static void main(String [] args){ //created main method
+	    
+	    System.out.println("Enter a String");
+	    String[] Array = {"uma","shanti","navin","prem","satya"};//taken 5 string elements
+	    
+	    }
+private char[][] Size;
+	    public void search() throws Exception{//taking a method search element
+
+	    	Scanner sc = new Scanner(System.in);
+		    String name=sc.next(); //taken scanner to give input
+		    
+		  
+		    
+			System.out.println(Size[1]);//showing how the exception handles
+		    System.out.println("value found");
+		     
+		   try{   
+	    }
+	    catch (Exception e){
+	    	
+	    return;
+	    
+	    }   
+	    }
+}
+
+
